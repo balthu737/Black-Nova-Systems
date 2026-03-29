32cc34d0d3fa5775602c8d45d7b8339ed8999f9e
lucardaci <lucacardaci1@gmail.com>
2026-03-29 17:46:11 -0300

    -_-

--- backend/controladores/index.js
+++ backend/controladores/index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const homeRoutes = require("./src/routes/home");
 const userRoutes = require("./src/routes/user");
+const appsRoutes = require("./src/routes/apps");
 
 app.use("/", homeRoutes);
 app.use("/", userRoutes);
-
+app.use("/", appsRoutes);
 
 app.listen(3000, () => {
   console.log('Servidor corriendo en http://localhost:3000');
