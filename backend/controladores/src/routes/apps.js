c1fda3f0e507aaae7bed14f313ecd6d975dbeb89
lucardaci <lucacardaci1@gmail.com>
2026-04-02 19:07:09 -0300

    -_-

--- backend/controladores/src/routes/apps.js
+++ backend/controladores/src/routes/apps.js
@@ -2,8 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/appsController");
 
-router.post("/login", userController.login);
-router.post("/create", userController.create);
-router.post("/edit", userController.edit);
+router.post("/view", appsController.login);
+
 
 module.exports = router;
