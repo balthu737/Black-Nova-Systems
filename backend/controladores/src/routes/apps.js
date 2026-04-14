915e02b01f5f4d50fbbb65e6d92ac6f24ec25a1b
balthu <balthu737@gmail.con>
2026-04-13 21:01:37 -0300

    .-.

--- backend/controladores/src/routes/apps.js
+++ backend/controladores/src/routes/apps.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/appsController");
 
-router.post("/view", appsController.view);/*alejo sos un come pitos anda a entrenar*/
+router.post("/view", appsController.view);
 router.post("/view", appsController.redirect);
 
 module.exports = router;
