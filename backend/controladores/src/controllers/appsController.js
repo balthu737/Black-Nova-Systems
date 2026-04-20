c450d4cd5e351ab3ee04299d0e1226468ab38bb6
lucardaci <lucacardaci1@gmail.com>
2026-04-20 11:49:29 -0300

    filtros de apps y herramientas para mantenimiento

--- backend/controladores/src/controllers/appsController.js
+++ backend/controladores/src/controllers/appsController.js
@@ -32,8 +32,56 @@ exports.redirect = (req, res)=>{
 exports.validateUser = (req, res)=>{
     const validate= user.login(req);
     if(validate){
-        res.send('ss');
+        res.send('success');
     }else{
         res.send('user not registred');
     }
+}
+exports.filter = (req, res)=>{
+    const{id,name}=req.body;
+    db.query(
+        "CALL getApps(?,?)",[id, name],
+        (err, results) => {
+            if(err){
+                return res.status(500).send("Conection error");
+            }
+            if (results.length >0){
+                res.send('Obtained apps',results);
+            }else {
+                res.send('Similarties not found');
+            }
+        }
+    )
+}
+exports.performMaintenance = (req, res)=>{
+    const {id}=req.body;
+    db.query(
+        "CALL maintenanceApps(?,1)",[id],
+        (err, results) => {
+            if(err){
+                return res.status(500).send("Conection error");
+            }
+            if (results.length >0){
+                res.send('perform maintenance',results);
+            }else {
+                res.send('maintenance not avaible');
+            }
+        }
+    )
+}
+exports.finalizeMaintenence = (req, res)=>{
+    const {id}=req.body;
+    db.query(
+        "CALL maintenanceApps(?,0)",[id],
+        (err, results) => {
+            if(err){
+                return res.status(500).send("Conection error");
+            }
+            if (results.length >0){
+                res.send('maintenance finalized',results);
+            }else {
+                res.send('maintenance not avaible to finalize');
+            }
+        }
+    )
 }
--- backend/controladores/src/routes/apps.js
+++ backend/controladores/src/routes/apps.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const userController = require("../controllers/appsController");
+const appsController = require("../controllers/appsController");
 
 router.post("/view", appsController.view);/*alejo sos un come pitos anda a entrenar*/
 router.post("/view", appsController.redirect);
+router.post("/validateUser", appsController.validateUser);
+router.post("/filter", appsController.filter);
+router.post("/maintenanceOn", appsController.performMaintenance);
+router.post("/maintenanceOff", appsController.finalizeMaintenence);
 
 module.exports = router;
